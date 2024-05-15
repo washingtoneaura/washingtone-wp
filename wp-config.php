c1d6cd65bbc3e782c95bfb6b76297dab62f3a500
Washingtone Aura <aurawashingtone@gmail.com>
2024-05-15 13:09:12 +0000

    updates

--- wp-config.php
+++ wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'washingtone_wp_db' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'washingtone' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'washingtone' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
